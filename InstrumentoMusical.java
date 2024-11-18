95023bc7d2f776702bb5439823a88d1530090631
omartinin <omartinin@gmail.com>
2024-11-18 13:08:40 +0100

    Commit inicial

---
+++ InstrumentoMusical.java
@@ -0,0 +1,53 @@
+public class InstrumentoMusical {
+    // Nombre del instrumento
+    private String nombre;
+    // Año de fabricación
+    private int anoDeFabricacion;
+    // Indica si el instrumento es Electrónico
+    private boolean electronico;
+
+    public InstrumentoMusical(String nombre, boolean electronico) {
+        this.nombre = nombre;
+        this.anoDeFabricacion = 2007;
+        this.electronico = electronico;
+    }
+    /**
+     * Devuelve el nombre del instrumento
+     */
+    public String getNombre() {
+        return nombre;
+    }
+    /**
+     * Devuelve el año de fabricación del instrumento
+     */
+    public int getAnoDeFabricacion() {
+        return anoDeFabricacion;
+    }
+    /**
+     * Devuelve el tipo de construcción del instrumento
+     */
+    public boolean getElectronico() {
+        return electronico;
+    }
+    /**
+     * Disminulle el año de fabricación para que el instrumento envejezca
+     */
+    public void envejecerInstrumento(int anosEnvejecidos) {
+        anoDeFabricacion -= anosEnvejecidos;
+    }
+    /**
+     * Cambia el tipo de construcción del instrumento
+     */
+    public void invertirElectronico() {
+        electronico = !electronico;
+    }
+
+
+
+
+
+
+
+
+
+}
